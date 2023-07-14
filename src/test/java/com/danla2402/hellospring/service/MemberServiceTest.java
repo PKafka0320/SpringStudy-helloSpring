1fb7bd49c74eb38824c0a39cd72d1c66ac132592
Seonghwan <aiya0422@gmail.com>
2023-07-14 18:40:45 +0900

    create member service test, edit member service for DI

---
+++ src/test/java/com/danla2402/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,73 @@
+package com.danla2402.hellospring.service;
+
+import com.danla2402.hellospring.domain.Member;
+import com.danla2402.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member result = memberService.findOne(saveId).get();
+        assertThat(result.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member);
+
+        //then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        /*try {
+            memberService.join(member2);
+            fail("같은 이름의 회원은 생길 수 없어야 합니다.");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }*/
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
