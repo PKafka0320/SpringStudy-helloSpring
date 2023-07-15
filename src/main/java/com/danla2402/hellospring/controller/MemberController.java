e37ea582adc7d35b81d7eb6b87aa68a004403302
Seonghwan <aiya0422@gmail.com>
2023-07-15 15:23:20 +0900

    component scan, autowired depedency

---
+++ src/main/java/com/danla2402/hellospring/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.danla2402.hellospring.controller;
+
+import com.danla2402.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
