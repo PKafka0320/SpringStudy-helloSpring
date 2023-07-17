29f5a9f80b4c65aa08664d980e0a1dc83fc0c3fd
Seonghwan <aiya0422@gmail.com>
2023-07-17 13:48:55 +0900

    create JdbcMemberRepository, edit Spring config & properties, fix param of repository interface

--- src/main/java/com/danla2402/hellospring/SpringConfig.java
+++ src/main/java/com/danla2402/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.danla2402.hellospring;
 
+import com.danla2402.hellospring.repository.JdbcMemberRepository;
 import com.danla2402.hellospring.repository.MemberRepository;
 import com.danla2402.hellospring.repository.MemoryMemberRepository;
 import com.danla2402.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
