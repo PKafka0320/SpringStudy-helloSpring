7b238ea6f128f860ac0e4f50e982e34e6f6fbe49
Seonghwan <aiya0422@gmail.com>
2023-07-17 21:42:21 +0900

    apply AOP

--- src/main/java/com/danla2402/hellospring/service/MemberService.java
+++ src/main/java/com/danla2402/hellospring/service/MemberService.java
@@ -22,18 +22,10 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        long start = System.currentTimeMillis();
-
-        try {
-            //중복된 이름의 회원 가입 방지
-            validateDuplicateMember(member); //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        //중복된 이름의 회원 가입 방지
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
