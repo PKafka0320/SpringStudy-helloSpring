607af45523d8011a6f266e86582b9d941066a016
Seonghwan <aiya0422@gmail.com>
2023-07-17 21:29:30 +0900

    add logging in service-join

--- src/main/java/com/danla2402/hellospring/service/MemberService.java
+++ src/main/java/com/danla2402/hellospring/service/MemberService.java
@@ -22,10 +22,18 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        //중복된 이름의 회원 가입 방지
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            //중복된 이름의 회원 가입 방지
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
