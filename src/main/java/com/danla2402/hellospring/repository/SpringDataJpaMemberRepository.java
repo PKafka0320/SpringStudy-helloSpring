c2dcca2719c9e9e2d28cbd00278ce628159a4a0e
Seonghwan <aiya0422@gmail.com>
2023-07-17 21:18:44 +0900

    create spring data jpa repository

---
+++ src/main/java/com/danla2402/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package com.danla2402.hellospring.repository;
+
+import com.danla2402.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    @Override
+    Optional<Member> findByName(String name);
+}
